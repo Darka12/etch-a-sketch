11250e9e172a5dece1a44625405dbbc3d06763ce
Oliver Horwood <thereviloproject@gmail.com>
2021-08-14 15:29:14 +0100

    black colour

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.getElementById("container");
 
+
+
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
+    cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'))
   };
 };
 
 makeRows(16, 16);
+
