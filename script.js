89e846932468e62c6eb460b11f0fc0ba70a2351d
Oliver Horwood <thereviloproject@gmail.com>
2021-08-14 17:03:11 +0100

    added a clear grid function and a button that executes it (minor styling as well)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container");
+const resetBtn = document.getElementById('reset-button');
+
 
 function createGrid() {
   for (let i = 0; i < 256; i++) {
@@ -14,9 +16,19 @@ function createColors() {
   gridCells.forEach(cell => {
     cell.addEventListener('mouseover', function() {
       cell.style.background = 'black';
+      cell.style.border = '1px solid black';
     })
   })
 }
 
+function resetGrid() {
+  let gridCells = document.querySelectorAll('div.cellClass');
+  gridCells.forEach(cell => {
+    cell.style.background = 'white';
+  })
+  
+}
+
 createGrid()
 createColors()
+
