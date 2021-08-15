ae162779317b7ae0e99a51bb624c321b8c3ebe9e
Oliver Horwood <thereviloproject@gmail.com>
2021-08-15 04:06:35 +0100

    added a fucntion that generates a random rgb color

--- script.js
+++ script.js
@@ -51,7 +51,12 @@ function newGrid() {
 }
 
 function rgbMode() {
-  
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  const rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string
+  console.log(rgb);
 }
 
 createGrid()
